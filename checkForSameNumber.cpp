b655e2fb2251c82b510ebf1b57a7a673f6d6d573
atanasova07 <97397646+atanasova07@users.noreply.github.com>
2022-01-20 22:33:22 +0200

    Check all for the same number

--- checkForSameNumber.cpp
+++ checkForSameNumber.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 const int SIZE_OF_SUDOKU = 9;
-const int SIZE_3X3 = 3;
+const int SIZE_OF_3X3 = 3;
 
 bool checkTheRowForSameElement(char** sudoku, int row, char num) {
 	for (int j = 0; j < SIZE_OF_SUDOKU; j++) {
