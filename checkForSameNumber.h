2055b73a21416a08da328f4ebac32e26542eef49
atanasova07 <adriana0702@abv.bg>
2022-01-20 22:16:06 +0200

    Header file for checking for same number

--- checkForSameNumber.h
+++ checkForSameNumber.h
@@ -1,4 +1,7 @@
 #pragma once
 
 bool checkTheRowForSameElement(char** sudoku, int row, char num);
+bool checkTheColumnForSameElement(char** sudoku, int position, char num);
+bool check3x3(char** sudoku, int row, int position, char num);
+bool checkAll(char** sudoku, int row, int position, char num);
 int main();
