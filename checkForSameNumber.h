cca75fbcf8642e2e916e889a7ab91094d8725142
atanasova07 <adriana0702@abv.bg>
2022-01-22 17:25:02 +0200

    Added comment block

--- checkForSameNumber.h
+++ checkForSameNumber.h
@@ -1,3 +1,18 @@
+/**
+*
+* Solution to course project # 6
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2021/2022
+*
+* @author Adriana Atanasova
+* @idnumber 6MI0600018
+* @compiler VC
+*
+* <Header file for checking the rules for repeatin number in row, column, 3x3 square>
+*
+*/
+
 #pragma once
 
 bool checkTheRowForSameElement(char** sudoku, int row, char num);
