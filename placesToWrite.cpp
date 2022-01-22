e28c38d7615f52dc0ba9f42e63e6d1cb05fea991
atanasova07 <adriana0702@abv.bg>
2022-01-22 18:42:41 +0200

    Added comment block"

--- placesToWrite.cpp
+++ placesToWrite.cpp
@@ -1,5 +1,21 @@
+/**
+*
+* Solution to course project # 6
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2021/2022
+*
+* @author Adriana Atanasova
+* @idnumber 6MI0600018
+* @compiler VC
+*
+* <Function that count places that need to be filled>
+*
+*/
+
 #include <iostream>
 #include "placesToWrite.h"
+
 using namespace std;
 
 const int SIZE_OF_SUDOKU = 9;
