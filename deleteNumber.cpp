f6639792f90ed8f2f7bef79af12ccde884df3bf4
atanasova07 <adriana0702@abv.bg>
2022-01-22 18:31:31 +0200

    Added comment block

--- deleteNumber.cpp
+++ deleteNumber.cpp
@@ -1,3 +1,18 @@
+/**
+*
+* Solution to course project # 6
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2021/2022
+*
+* @author Adriana Atanasova
+* @idnumber 6MI0600018
+* @compiler VC
+*
+* <Function that check if it is possible to delete a number>
+*
+*/
+
 #include <iostream>
 #include "deleteNumber.h"
 
