a6a0b70fc60123f233e635a2864a3f2fc5b48cc6
atanasova07 <adriana0702@abv.bg>
2022-01-22 18:40:24 +0200

    Added comment block

--- medium.cpp
+++ medium.cpp
@@ -1,3 +1,18 @@
+/**
+*
+* Solution to course project # 6
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2021/2022
+*
+* @author Adriana Atanasova
+* @idnumber 6MI0600018
+* @compiler VC
+*
+* <Function that read from file - open and print medium sudoku>
+*
+*/
+
 #include <iostream>
 #include <fstream>
 #include "medium.h"
