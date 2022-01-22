2d909deca29030ee9f099dede0d366e0a308a035
atanasova07 <adriana0702@abv.bg>
2022-01-22 17:33:31 +0200

    Added comment block

--- copySudoku.cpp
+++ copySudoku.cpp
@@ -1,3 +1,18 @@
+/**
+*
+* Solution to course project # 6
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2021/2022
+*
+* @author Adriana Atanasova
+* @idnumber 6MI0600018
+* @compiler VC
+*
+* <Copy the original sudoku - without modifications>
+*
+*/
+
 #include <iostream>
 #include <fstream>
 #include "copySudoku.h"
