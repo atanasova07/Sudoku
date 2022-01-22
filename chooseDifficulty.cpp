41b7f16142f57f2d7473e56dcb83cdbfd2382443
atanasova07 <adriana0702@abv.bg>
2022-01-22 17:27:03 +0200

    Added comment block

--- chooseDifficulty.cpp
+++ chooseDifficulty.cpp
@@ -1,3 +1,18 @@
+/**
+*
+* Solution to course project # 6
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2021/2022
+*
+* @author Adriana Atanasova
+* @idnumber 6MI0600018
+* @compiler VC
+*
+* <Function for choosing the difficulty of the game>
+*
+*/
+
 #include <iostream>
 #include "chooseDifficulty.h"
 #include "easy.h"
